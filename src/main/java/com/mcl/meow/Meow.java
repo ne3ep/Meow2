2e1316eca03e54dbdc951f55fe7a09380beb5a13
McLem0nPlay <129785581+McLem0nPlay@users.noreply.github.com>
2025-04-11 13:30:10 +0300

    Add files via upload

---
+++ src/main/java/com/mcl/meow/Meow.java
@@ -0,0 +1,18 @@
+package com.mcl.meow;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Meow implements ModInitializer {
+	public static final String MOD_ID = "meow";
+
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("бляздец");
+	}
+}
---
+++ src/main/java/com/mcl/meow/MeowClient.java
@@ -0,0 +1,10 @@
+package com.mcl.meow;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MeowClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
